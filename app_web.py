4715d09be5934b93ccb00ed9bcdc38c31b6851fb
cbrancone <130234505+cbrancone@users.noreply.github.com>
2026-08-03 11:47:57 +0200

    Update app_web.py

--- app_web.py
+++ app_web.py
@@ -18,7 +18,7 @@ st.set_page_config(
 # ==========================================
 @st.cache_resource
 def init_supabase() -> Client:
-    url = st.secrets["https://autonoleggio-app-kujmsvy8gen5we7emmqxnv.streamlit.app/"]
+    url = "https://autonoleggio-app-kujmsvy8gen5we7emmqxnv.streamlit.app/"
     key = st.secrets["SUPABASE_KEY"]
     return create_client(URL, key)
 
