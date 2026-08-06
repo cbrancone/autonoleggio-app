b7b10c910bf653f6e0fbf13cecbde60258418748
cbrancone <130234505+cbrancone@users.noreply.github.com>
2026-08-06 12:33:06 +0200

    Update web_autonoleggio.py

--- web_autonoleggio.py
+++ web_autonoleggio.py
@@ -11,7 +11,7 @@ APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwyQyyVasHzYp0OPPdXj0
 
 # 2. URL del tuo foglio Google per l'esportazione in CSV (sostituisci l'ID del foglio)
 # Nota: Il foglio deve essere impostato su "Chiunque abbia il link può visualizzare"
-SPREADSHEET_ID = "https://docs.google.com/spreadsheets/d/1-piIVsHAWbxwTHYf7imtnvx_MUoit-af4YahnncVF7Y/edit?gid=0#gid=0"
+SPREADSHEET_ID = "https://docs.google.com/spreadsheets/d/1hktUuKCvomCuvhbklmXs-A93usdhdouC/edit?usp=drive_link&ouid=114374870114697389870&rtpof=true&sd=true"
 CSV_URL = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}/export?format=csv"
 
 # ---------------------------------------------------------
